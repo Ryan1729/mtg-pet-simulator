eb55237ff1683b6dfb6e694223a9f4f2b737312e
Ryan1729 <Ryan1729@gmail.com>
2024-10-21 01:06:00 -0600

    add rest of the deck

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,46 @@ use simulate::Spec::*;
 fn main() {
     // TODO accept as input
     let deck = vec![
+        InsatiableAvarice,
+        SchemingSymmetry,
+        SchemingSymmetry,
+        FeedTheSwarm,
+        SignInBlood,
+        StarscapeCleric,
+        StarscapeCleric,
+        WishclawTalisman,
+        WishclawTalisman,
+        WishclawTalisman,
+        WishclawTalisman,
+        CeaseAndDesist,
+        CeaseAndDesist,
+        CeaseAndDesist,
+        CeaseAndDesist,
+        HowlingMine,
+        HowlingMine,
+        JetMedallion,
+        MindStone,
+        MindStone,
+        MindStone,
+        MindStone,
+        GrimTutor,
+        HoodedBlightfang,
+        NighthawkScavenger,
+        ToxicDeluge,
+        VitoThornOfTheDuskRose,
+        VitoThornOfTheDuskRose,
+        VitoThornOfTheDuskRose,
+        BakeIntoThePie,
+        EnduringTenacity,
+        HagraMauling,
+        SheoldredTheApocalypse,
+        ExquisiteBlood,
+        ExquisiteBlood,
+        ExquisiteBlood,
+        ExquisiteBlood,
+        MemorialToFolly,
+        MemorialToFolly,
+        PhyrexianTower,
         Swamp,
         Swamp,
         Swamp,
@@ -18,8 +58,15 @@ fn main() {
         Swamp,
         Swamp,
         Swamp,
-        // TODO rest of deck
+        TheDrossPits,
+        TheDrossPits,
+        TheDrossPits,
+        TheDrossPits,
+        BlastZone,
+        SceneOfTheCrime,
     ];
+
+    assert_eq!(deck.len(), 60);
 
     let outcome = simulate::calculate(NthDraw(0), &deck);
 
