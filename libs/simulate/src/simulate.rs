f046d9b62e6b4da0ffb47798a23338362d18152a
Ryan1729 <Ryan1729@gmail.com>
2024-10-23 01:06:00 -0600

    start fleshing out calculation

--- libs/simulate/src/simulate.rs
+++ libs/simulate/src/simulate.rs
@@ -1,19 +1,72 @@
 use card::Card::{self, *};
 use mana::ManaType::*;
 
-pub enum Spec {
-    //AllDraws,
-    NthDraw(usize),
-}
+type PermutationNumber = usize;
 
 #[derive(Debug)]
-pub struct Outcome {
-    // TODO useful fields
+pub enum Spec {
+    //AllDraws,
+    NthDraw(PermutationNumber),
+}
+use Spec::*;
+
+#[derive(Debug)]
+pub enum Outcome {
+    Lose,
+    Win,
+}
+use Outcome::*;
+
+#[derive(Debug)]
+pub enum CalculateError {
+    DeckTooLarge,
+}
+use CalculateError::*;
+
+const MAX_DECK_SIZE: u8 = 60;
+
+pub fn calculate(spec: Spec, deck: &[Card]) -> Result<Outcome, CalculateError> {
+    if deck.len() > MAX_DECK_SIZE as _ {
+        return Err(DeckTooLarge);
+    }
+
+    let ordered_deck = match spec {
+        NthDraw(n) => {
+            nth_ordered(deck, n)
+        }
+    }?;
+    dbg!(&ordered_deck);
+
+    let mut hand = Vec::with_capacity(16);
+
+    let mut deck = ordered_deck;
+
+    while hand.len() < 7 {
+        match draw(deck) {
+            None => {
+                return Ok(Lose);
+            },
+            Some((d, card)) => {
+                hand.push(card);
+                deck = d;
+            }
+        }
+    }
+
+    todo!();
 }
 
-pub fn calculate(spec: Spec, right: &[Card]) -> Outcome {
-    // TODO actual impl
-    Outcome {}
+type Deck = Box<[Card]>;
+
+fn draw(deck: Deck) -> Option<(Deck, Card)> {
+    todo!();
+}
+
+fn nth_ordered(
+    deck: &[Card],
+    n: PermutationNumber
+) -> Result<Deck, CalculateError> {
+   todo!()
 }
 
 #[cfg(test)]
